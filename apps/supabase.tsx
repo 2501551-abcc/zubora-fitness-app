4f8159eb612b77d28e705296736e033238729d55
2501168abcc <2501168@s.asojuku.ac.jp>
2026-07-16 11:07:38 +0900

    Delete supabase.tsx

--- apps/supabase.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://lihwukvbugvtbevbbbeg.supabase.co/'; // ★Project URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpaHd1a3ZidWd2dGJldmJiYmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NzAwNzksImV4cCI6MjA5OTI0NjA3OX0.2DzIHb2Hs6iCqDdq80CBVko5QSx6ArQg_BxXayAb9xU';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
